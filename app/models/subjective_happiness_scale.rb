06994d368689dc09e0fc1b7e1005ad7a5fbf7d8d
rgpass <rgpass@gmail.com>
2014-07-08 18:01:04 -0400

    Troubleshoot schema

---
+++ app/models/subjective_happiness_scale.rb
@@ -0,0 +1,2 @@
+class SubjectiveHappinessScale < ActiveRecord::Base
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140706142438) do
+ActiveRecord::Schema.define(version: 20140708182117) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "subjective_happiness_scales", force: true do |t|
+    t.integer  "general"
+    t.integer  "peers"
+    t.integer  "regardless"
+    t.integer  "not"
+    t.decimal  "score"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "first_name"
