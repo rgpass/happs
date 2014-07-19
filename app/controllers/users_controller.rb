616a9f3cce0c2c80cf8aff4f8bed8e59074e7cd7
rgpass <rgpass@gmail.com>
2014-07-19 18:55:02 -0400

    Begin editing show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to Happs!"
+      flash[:success] = "Welcome to Happs! If you haven't already, we suggest taking the tour."
       # redirect_to @user
       redirect_to root_url
     else
