3b2e919ca0ac236793f56719c0fee3c516035a09
rgpass <rgpass@gmail.com>
2014-07-21 11:06:28 -0400

    Setting up SSL

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,4 +11,12 @@ class StaticPagesController < ApplicationController
 
   def research
   end
+
+  def ssl_validation
+	  send_file(
+	    "#{Rails.root}/public/15CAA848590025BB3B93F642928894E0.txt",
+	    filename: "15CAA848590025BB3B93F642928894E0.txt",
+	    type: "text/plain"
+	  )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Happs::Application.routes.draw do
   get "examples/ohq"
   get "examples/activity"
 
+  match "/15CAA848590025BB3B93F642928894E0.txt", to: "static_pages#ssl_validation", via: 'get'
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
   resources :subjective_happiness_scales
