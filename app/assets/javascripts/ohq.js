737352b4d1960bc70c75ad35890a9bdbb92584f8
rgpass <rgpass@gmail.com>
2014-08-01 10:09:57 -0400

    Add front end validation to OHQ

---
+++ app/assets/javascripts/ohq.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+var numberOfQuestions = $('label').length / 6;
+
+$('label').on('click', function() {
+	setTimeout(function() {
+		updateButton($('label.active').length);
+	}, 250);
+});
+
+function updateButton(selectedCount) {
+	if (selectedCount < numberOfQuestions) {
+		$('input[type=submit]').val(selectedCount + " of " + numberOfQuestions + " answered");
+	} else {
+		$('input[type=submit]').val("All Done").attr('disabled',false);
+	}
+}
+
+});
