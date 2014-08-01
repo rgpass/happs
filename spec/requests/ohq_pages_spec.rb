8611c38d4edbe7e4283c2f4f5fdfb24f3d17f254
rgpass <rgpass@gmail.com>
2014-08-01 13:13:23 -0400

    Update OHQ spec

--- spec/requests/ohq_pages_spec.rb
+++ spec/requests/ohq_pages_spec.rb
@@ -14,47 +14,47 @@ describe "ohq_pages" do
 		it { should have_title("Quantify Happiness") }
 		it { should have_selector("h1", "Oxford Happiness Questionnaire") }
 
+		describe "invalid information" do
+			it { should have_css(".btn.btn-primary[disabled]") }
+			it { should_not have_content("All Done") }
+		end
+
 		pending "Uncomment if you want to test posting"
 		# describe "create ohq POST /ohqs" do
-
-		# 	let(:submit) { "All Done" }
-
 		# 	describe "valid information" do
 		# 		before do
-		# 			choose('ohq_q1_5')
-		# 			choose('ohq_q2_5')
+		# 			choose('ohq_q1_6')
+		# 			choose('ohq_q2_6')
 		# 			choose('ohq_q3_6')
-		# 			choose('ohq_q4_4')
-		# 			choose('ohq_q5_5')
+		# 			choose('ohq_q4_6')
+		# 			choose('ohq_q5_6')
 		# 			choose('ohq_q6_6')
 		# 			choose('ohq_q7_6')
-		# 			choose('ohq_q8_5')
+		# 			choose('ohq_q8_6')
 		# 			choose('ohq_q9_6')
-		# 			choose('ohq_q10_5')
+		# 			choose('ohq_q10_6')
 		# 			choose('ohq_q11_6')
 		# 			choose('ohq_q12_6')
-		# 			choose('ohq_q13_5')
+		# 			choose('ohq_q13_6')
 		# 			choose('ohq_q14_6')
 		# 			choose('ohq_q15_6')
 		# 			choose('ohq_q16_6')
-		# 			choose('ohq_q17_5')
+		# 			choose('ohq_q17_6')
 		# 			choose('ohq_q18_6')
 		# 			choose('ohq_q19_6')
 		# 			choose('ohq_q20_6')
-		# 			choose('ohq_q21_5')
+		# 			choose('ohq_q21_6')
 		# 			choose('ohq_q22_6')
 		# 			choose('ohq_q23_6')
 		# 			choose('ohq_q24_6')
-		# 			choose('ohq_q25_5')
-		# 			choose('ohq_q26_5')
+		# 			choose('ohq_q25_6')
+		# 			choose('ohq_q26_6')
 		# 			choose('ohq_q27_6')
-		# 			choose('ohq_q28_5')
+		# 			choose('ohq_q28_6')
 		# 			choose('ohq_q29_6')
 		# 		end
 
-		# 		it "creates ohq record" do
-		# 			expect { click_button submit }.to change(Ohq, :count).by(1)
-		# 		end
+		# 		let(:submit) { "All Done" }
 
 		# 		it "creates user.ohq" do
 		# 			expect { click_button submit }.to change(user.ohqs, :count).by(1)
@@ -66,19 +66,7 @@ describe "ohq_pages" do
 		# 			it { should have_title(full_name(user)) }
 		# 			it { should have_selector('div.alert.alert-success',
 		# 				text: 'Happiness tracked!') }
-		# 		end
-		# 	end
-
-		# 	describe "with invalid information" do
-		# 		it "does not create ohq" do
-		# 			expect { click_button submit }.not_to change(Ohq, :count)
-		# 		end
-
-		# 		describe "after submission" do
-		# 			before { click_button submit }
-
-		# 			it { should have_title('Quantify Happiness') }
-		# 			it { should have_content('error') }
+		# 			it { should have_content("100") }
 		# 		end
 		# 	end
 		# end
