f0acac73d5ac30fff8a683dcfccc9af8aa73a0a0
rgpass <rgpass@gmail.com>
2014-08-08 17:20:05 -0400

    Add PAFD request spec

---
+++ spec/requests/pafd_pages_spec.rb
@@ -0,0 +1,26 @@
+require "spec_helper"
+
+describe "pafd_pages" do
+
+	subject { page }
+
+	describe "new pafd GET /pafds/new" do
+		let(:user) { FactoryGirl.create(:user) }
+		before do
+			sign_in user
+			visit new_pafd_path
+		end
+
+		it { should have_title("Person-Activity Fit") }
+		it { should have_selector("h1", "Person-Activity Fit Diagnostic") }
+
+		describe "invalid information" do
+			it { should have_css(".btn.btn-primary[disabled]") }
+			it { should_not have_content("All Done") }
+		end
+
+		describe "valid information" do
+			pending "Add once test suite is sped up"
+		end
+	end
+end
